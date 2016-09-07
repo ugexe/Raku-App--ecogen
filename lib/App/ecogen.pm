60a8fb27f581224cb9c0a0e837849548066558b8
Nick Logan <ugexe@cpan.org>
2016-09-06 20:49:39 -0400

    Add cpan PSIXDISTS index

--- lib/App/ecogen.pm
+++ lib/App/ecogen.pm
@@ -1,1 +1,20 @@
-unit module ecogen;
+unit module App::ecogen;
+
+sub from-json($text) is export {
+    INIT my $INTERNAL_JSON = (so try { ::("Rakudo::Internals::JSON") !~~ Failure }) == True;
+    $INTERNAL_JSON
+        ?? ::("Rakudo::Internals::JSON").from-json($text)
+        !! do {
+            my $a = ::("JSONPrettyActions").new();
+            my $o = ::("JSONPrettyGrammar").parse($text, :actions($a));
+            JSONException.new(:$text).throw unless $o;
+            $o.ast;
+        }
+}
+
+sub to-json(|c) is export {
+    INIT my $INTERNAL_JSON = (so try { ::("Rakudo::Internals::JSON") !~~ Failure }) == True;
+    $INTERNAL_JSON
+        ?? ::("Rakudo::Internals::JSON").to-json(|c)
+        !! &::("to-json").(|c);
+}
